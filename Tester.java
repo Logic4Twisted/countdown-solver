395d47d97a36bba2c62a9cdc8f3200e2494035e3
Zaim Musa <zaim.musa@booking.com>
2020-12-19 14:41:54 +0100

    Tester picks random targets

--- Tester.java
+++ Tester.java
@@ -3,7 +3,9 @@ import java.util.*;
 public class Tester {
 
 	public static void main(String[] args) {
-		int target = 936;
+
+		Random r = new Random();
+		int target = 100 + r.nextInt(900);
 		int[] nums = {75, 25, 6, 7 , 8 , 9};
 		Solver solver = new Solver(target, nums);
 
@@ -12,6 +14,9 @@ public class Tester {
 		if (solver.isSolutionFound()) {
 			System.out.println("It is possible");
 			for (String str : solver.getSolutionSet()) {
+
+				int result = Evaluator.eval(str);
+				if (result != target) System.out.println("!!!!!!  What, this is not correct !!!!");
 				
 				System.out.println(str + " = " + Evaluator.eval(str));
 
